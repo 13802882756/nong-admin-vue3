719323ba1f6fe4ea98116eef1fc0be492e70062a
zengfr1 <zengfr1@xiaopeng.com>
2023-12-15 17:45:46 +0800

    修改构建文件的output

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,14 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/main' : '/',
+  base: process.env.NODE_ENV === "production" ? "/main" : "/",
   plugins: [vue()],
+  // define: {
+  //   'process.env.NODE_ENV': 'production'
+  // },
+  build: {
+    outDir: "main",
+  },
   server: {
     headers: {
       // 重点1: 允许跨域访问子应用页面
