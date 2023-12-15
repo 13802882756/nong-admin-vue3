a4f957ec34d3c2ca095d39169f30d9fb4969728e
zengfr1 <zengfr1@xiaopeng.com>
2023-12-15 18:49:22 +0800

    feat: 在打包路径中添加dist

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
 import { fileURLToPath, URL } from "node:url";
-
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/main" : "/",
+  base: process.env.NODE_ENV === "production" ? "/main/dist" : "/",
   plugins: [vue()],
   // define: {
   //   'process.env.NODE_ENV': 'production'
   // },
   build: {
-    outDir: "main",
+    outDir:  path.resolve(__dirname, './main/dist'),
   },
   server: {
     headers: {
